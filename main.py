780bfbf572e711414f20d6f96b4b3ee301ab4899
fadeoclock <modfadeoclock@gmail.com>
2025-03-21 05:38:52 +0500

    depl

--- main.py
+++ main.py
@@ -4,9 +4,23 @@ import firebase_admin
 import os
 from firebase_admin import credentials, storage
 from moviepy import VideoFileClip, concatenate_videoclips
+from dotenv import load_dotenv
+load_dotenv()  # Load environment variables from .env file
 
 # Initialize Firebase Admin SDK
 cred = credentials.Certificate('ballebaaz-74803.json')
+cred = credentials.Certificate({
+    "type": os.getenv('FIREBASE_TYPE'),
+    "project_id": os.getenv('FIREBASE_PROJECT_ID'),
+    "private_key_id": os.getenv('FIREBASE_PRIVATE_KEY_ID'),
+    "private_key": os.getenv('FIREBASE_PRIVATE_KEY').replace('\\n', '\n'),
+    "client_email": os.getenv('FIREBASE_CLIENT_EMAIL'),
+    "client_id": os.getenv('FIREBASE_CLIENT_ID'),
+    "auth_uri": os.getenv('FIREBASE_AUTH_URI'),
+    "token_uri": os.getenv('FIREBASE_TOKEN_URI'),
+    "auth_provider_x509_cert_url": os.getenv('FIREBASE_AUTH_PROVIDER_CERT_URL'),
+    "client_x509_cert_url": os.getenv('FIREBASE_CLIENT_CERT_URL')
+})
 firebase_admin.initialize_app(cred, {
     'storageBucket': 'ballebaaz-74803.appspot.com'
 })
